3defeb3b67913d7fbf2b0a495a88fc6e620b82c9
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-02-23 19:23:02 +0300

    Telegram token was revoked, yep :P

--- kip_photo.py
+++ kip_photo.py
@@ -2,9 +2,9 @@ import sys, os, json, requests, re, wget, time, telegram
 from bs4 import BeautifulSoup
 from telegram.ext import Updater
 
-bot = telegram.Bot('368637401:AAF--D8PxjFv_EcxZSbVoxFYaE_MUA6U2Zo')
-tag = "Фото_КИП"
-channel_name = "@photo_KIP"
+bot = telegram.Bot('YOUR_TELEGRAM_TOKEN')
+tag = "INSTAGRAM_TAG"
+channel_name = "@TELEGRAM_CHANNEL_NAME"
 
 base_url = "https://instagram.com/explore/tags/"
 username_url ="https://www.instagram.com/p/"
@@ -33,30 +33,34 @@ def getter(url):
 	return result
 
 #Telegram 
+
 def start(bot, media_json, username):
-	caption_url = media_json["caption"]
+
+	media_text = media_json["edge_media_to_caption"]["edges"][0]["node"]["text"]
+	media_url = media_json["display_url"]
+
+	if (len(media_text)>201):
+		media_text = ""
 
 	if (media_json["is_video"]==False):
-		bot.sendPhoto(chat_id=channel_name, photo=media_json["thumbnail_src"], caption="От @"+username+"\n"+caption_url)
+		bot.sendPhoto(chat_id=channel_name, photo=media_json["thumbnail_src"], caption="By @"+username+"\n"+media_text)
 	
 	if (media_json["is_video"]==True):
-		bot.sendVideo(chat_id=channel_name, video=getter(username_url+media_json["code"])["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["video_url"], caption="От @"+username+"\n"+caption_url)
+		bot.sendVideo(chat_id=channel_name, video=getter(username_url+media_json["shortcode"])["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["video_url"], caption="От @"+username+"\n"+media_text)
 
 InputWork()
 
 while True:
 
 	result = getter(mainurl)
-	tag_page = result["entry_data"]["TagPage"][0]["tag"]
-	media = tag_page["media"]["nodes"]
-	checker = tag_page["media"]
+	tag_page = result["entry_data"]["TagPage"][0]["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"][0]["node"]
+	media_id = tag_page["id"]
 
-	if ((media[0]["id"]!=global_id) and (media[0]["id"]!=previous_global_id)):
-		OutWork(media[0]["id"],'DATA')
+	if ((media_id!=global_id) and (media_id!=previous_global_id)):
+		OutWork(media_id,'DATA')
 		InputWork()
-
-		code = media[0]["code"]
+		code = tag_page["shortcode"]
 		username = getter(username_url+code)["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["owner"]["username"]
-		start(bot,media[0],username)
+		start(bot,tag_page,username)
 
 	time.sleep(10)
