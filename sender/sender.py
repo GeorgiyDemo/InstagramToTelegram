aa24b516cf62e3ee53876a8874c925ad98610493
Georgiy Demo <demenchuk.george@protonmail.com>
2020-06-12 13:03:56 +0300

    Redis есть

--- sender/sender.py
+++ sender/sender.py
@@ -1,94 +1,133 @@
-import sys, os, json, requests, re, wget, time, telegram
+import sys
+import os
+import json
+import requests
+import redis
+import re
+import wget
+import time
+import telegram
 from bs4 import BeautifulSoup
 from telegram.ext import Updater
 
-#Вместо этого зафигачиваем Redis
-"""
-def InputWork():
-	global global_id
-	global previous_global_id
-	f = open('DATA')
-	tex = f.read().split(";")
-	global_id = tex[0]
-	previous_global_id = tex[1]
-	f.close()
+class Redis:
+    """
+    Класс для удобной работы с Redis
+    В Redis хранятся ассоциации: media_id -> ник пользователя
+    """
+    def __init__(self, r_passwd):
+        self.r_table = redis.Redis(host="redis", port=6379, decode_responses=True, db=0, password=r_passwd)
+    
+    def get_data(self):
+        """Получаем данные с таблицы, отдаем в виде dict"""
+        lst = []
+        for value in self.r_table.values():
+            lst.append(value)
+        return lst
 
-def OutWork(result,files):
-    f = open(files, 'w')
-    f.write(result+";"+global_id)
-    f.close()
-"""
-
-def getter(url):
-	res = requests.get(url)
-	soup = BeautifulSoup(res.text, "lxml")
-	script_tag = soup.find("script", text=re.compile("window\._sharedData"))
-	shared_data = script_tag.string.partition("=")[-1].strip(" ;")
-	result = json.loads(shared_data)
-	return result
-
-#Telegram 
-
-
-
+    def set_data(self, key, value):
+        """Записываем данные в таблицу"""
+        self.r_table.set(key, value)
+    
+    @property
+    def data(self):
+        return self.get_data()
+    
+    @data.setter
+    def data(self, ddict):
+        for sub in ddict.items():
+            self.set_data(*sub)
 
 class Instagram2Telegram:
-	"""Класс для перекидывания постов по тегу """
-	def __init__(self, token, tag, channel_name):
-		#Бот телеграма
-		self.bot = telegram.Bot(token)
-		#URL пользователя
-		self.username_url ="https://www.instagram.com/p/"
-		#URL общий
-		self.mainurl = "https://instagram.com/explore/tags/" + tag
-		self.channel_name = channel_name
-		#Получаем данные с Redis
-		InputWork()
+    """Класс для перекидывания постов по тегу """
 
-		while True:
-			processing()
-			time.sleep(10)
+    def __init__(self, token, tag, channel_name, r_passwd):
 
-	def processing(self):
-		"""
-		Вылавливание постов с инсты
-		"""
-		#Получаем данные с url
-		result = getter(mainurl)
-		tag_page = result["entry_data"]["TagPage"][0]["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"][0]["node"]
-		media_id = tag_page["id"]
+        #Соединение с redis
+        self.r_obj = Redis(r_passwd)
+        # Бот телеграма
+        self.bot = telegram.Bot(token)
+        # URL пользователя
+        self.username_url = "https://www.instagram.com/p/"
+        # URL общий
+        self.mainurl = "https://instagram.com/explore/tags/" + tag
+        self.channel_name = channel_name
+        
+        while True:
+            try:
+                processing()
+            except KeyError as e:
+                print("KeyError:", e, e.args)
+                continue
+            time.sleep(10)
 
-		if ((media_id!=global_id) and (media_id!=previous_global_id)):
-			#Добавляем в Redis
-			OutWork(media_id,'DATA')
-			InputWork()
-			code = tag_page["shortcode"]
-			username = getter(username_url+code)["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["owner"]["username"]
-			send_telegram(bot,tag_page,username)
+    def processing(self):
+        """
+        Вылавливание постов с инсты
+        """
+        #Получаем данные с Redis
+        media_ids = self.r_obj.data
 
+        # Получаем данные с url
+        result = self.getter(mainurl)
+        tag_page = result["entry_data"]["TagPage"][0]["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"][0]["node"]
+        media_id = tag_page["id"]
 
-	def send_telegram(bot, media_json, username):
+        #Если id файла нет в списке всех файлов, то значит это новый файл
+        if media_id not in media_ids:
+            
+            code = tag_page["shortcode"]
+            username = self.getter(username_url+code)["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["owner"]["username"]
+            
+            # Добавляем в Redis
+            self.r_obj.data = {media_id : username}
 
-		media_text = media_json["edge_media_to_caption"]["edges"][0]["node"]["text"]
-		media_url = media_json["display_url"]
+            #Кидаем пост в Telegram
+            send_telegram(tag_page, username)
 
-		if (len(media_text)>201):
-			media_text = ""
+    def getter(self, url):
+        """
+        Парсинг страницы с вложением 
+        """
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "lxml")
+        script_tag = soup.find("script", text=re.compile("window\._sharedData"))
+        shared_data = script_tag.string.partition("=")[-1].strip(" ;")
+        result = json.loads(shared_data)
+        return result
 
-		elif not media_json["is_video"]:
-			bot.sendPhoto(chat_id=channel_name, photo=media_json["thumbnail_src"], caption="By @"+username+"\n"+media_text)
-		
-		elif media_json["is_video"]:
-			bot.sendVideo(chat_id=channel_name, video=getter(username_url+media_json["shortcode"])["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["video_url"], caption="By @"+username+"\n"+media_text)
+    def send_telegram(self, media_json, username):
+        """
+        Отправка вложения в Telegram
+        """
+
+        media_text = media_json["edge_media_to_caption"]["edges"][0]["node"]["text"]
+        media_url = media_json["display_url"]
+
+        if (len(media_text) > 201):
+            media_text = ""
+
+        if not media_json["is_video"]:
+            caption = "By @"+username+"\n"+media_text
+            media_url = media_json["thumbnail_src"]
+            self.bot.sendPhoto(chat_id=self.channel_name, photo=media_url, caption=caption)
+
+        if media_json["is_video"]:
+            caption = "By @"+username+"\n"+media_text
+            
+            buf_data = getter(username_url+media_json["shortcode"])
+            media_url = buf_data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["video_url"]
+            
+            self.bot.sendVideo(chat_id=self.channel_name, video=media_url, caption=caption)
+
 
 
 def main():
-	token = os.getenv('TELEGRAM_TOKEN', None)
-	channel_name = os.getenv('TELEGRAM_CHANNELNAME', None)
-	tag = os.getenv('INSTAGRAM_TAGNAME', None)
-
-	obj = Instagram2Telegram(token, tag, channel_name)
-	
+    token = os.getenv('TELEGRAM_TOKEN', None)
+    channel_name = os.getenv('TELEGRAM_CHANNELNAME', None)
+    tag = os.getenv('INSTAGRAM_TAGNAME', None)
+    r_passwd = os.getenv('REDIS_PASSWORD', None)
+    Instagram2Telegram(token, tag, channel_name, r_passwd)
 
 if __name__ == "__main__":
-	main()
+    main()
